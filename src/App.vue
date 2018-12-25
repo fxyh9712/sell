6a1a80ca31a087efa1d1d57efbe635187a1b022f
fxyh9712 <1351534609@qq.com>
2018-12-25 22:30:56 +0800

    页面骨架开发完成

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,52 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    <v-header></v-header>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <router-link :to="{path:'/goods'}">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link :to="{path:'/ratings'}">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link :to="{path:'/seller'}">商家</router-link>
+      </div>
+    </div>
+    <router-view>
+
+    </router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import header from 'components/header/header.vue'
+
+  export default {
+    components: {
+      'v-header': header
+    }
+  }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylesheet">
+  @import "./common/stylus/mixin.styl"
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    // border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+    border-1px(rgba(7, 17, 27, 0.1))
+
+    .tab-item
+      flex: 1
+      text-align: center
+
+      & > a
+        display: block
+        font-size: 14px
+        color: rgb(77, 85, 93)
+
+        &.active
+          color: #ac1122
 </style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,32 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App'
-import router from './router'
+import goods from 'components/goods/goods'
+import ratings from 'components/ratings/ratings'
+import seller from 'components/seller/seller'
 
-Vue.config.productionTip = false
+import 'common/stylus/index.styl'
 
 /* eslint-disable no-new */
+Vue.use(VueRouter)
+
+const routes = [
+  {path: '/goods', component: goods},
+  {path: '/ratings', component: ratings},
+  {path: '/seller', component: seller}
+]
+
+const router = new VueRouter({
+  linkActiveClass: 'active',
+  routes
+})
+
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  render: h => h(App)
 })
+
+router.push('/goods')
