48254358aadfe2d8e0c573336019ee0c68c50eec
fxyh9712 <1351534609@qq.com>
2018-12-26 13:20:09 +0800

    Vue-resource应用

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import App from './App'
 import goods from 'components/goods/goods'
 import ratings from 'components/ratings/ratings'
@@ -11,6 +12,7 @@ import 'common/stylus/index.styl'
 
 /* eslint-disable no-new */
 Vue.use(VueRouter)
+Vue.use(VueResource)
 
 const routes = [
   {path: '/goods', component: goods},
